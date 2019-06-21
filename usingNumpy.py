364a505124cc8cb08f0a340d4abfcc12cbc6b579
fesabelilla <fesabelilla@gmail.com>
2019-06-21 12:33:23 +0600

    <>

--- usingNumpy.py
+++ usingNumpy.py
@@ -5,10 +5,13 @@ Created on Thu Jun 20 12:34:19 2019
 @author: Asus
 """
 
-import numpy 
-numpy.array([1,2,3])
 
-#import numpy as np
+#Numeric Python
+
+#import numpy 
+#numpy.array([1,2,3])
+
+import numpy as np
 #np.array([1,2,3])
 
 # only use array function in the numpy package 
@@ -26,3 +29,46 @@ dist = r*radians(12)
 
 # Print out dist
 print(dist)
+
+# NumPy Array calculation
+
+np_height = np.array([1.73,1.68,1.71])
+np_weight = np.array([65.4,59.2,63.6])
+
+bmi = np_weight/np_height ** 2
+
+print(bmi)
+
+print(bmi[2])
+
+print(bmi > 21) #it return bool values Ex. True or False
+
+print(bmi[bmi > 21]) # it return value 
+
+# different btw NumPy array and List
+
+python_list = [1,2,3]
+
+print(python_list + python_list)
+
+numpy_array = np.array([1,2,3])
+
+print(numpy_array + numpy_array)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
